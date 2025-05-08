012c7bcf5d88157f17dfe57a50d85f46f1519841
ahcomat <abdelra7man87@gmail.com>
2025-05-08 11:41:13 +0300

    Update documentation

--- docs/search/all_d.js
+++ docs/search/all_d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-<<<<<<< HEAD
   ['m_163',['m',['../class_mech_trap.html#a45f233faa9ac2dbdbdeda4a3028d0302',1,'MechTrap']]],
   ['m_164',['M',['../class_iso_hard.html#adfa14b43b1f81b2c5cc531b7acee714a',1,'IsoHard::M()'],['../class_transport_model.html#a81aef96bb4213714e09312044af65d52',1,'TransportModel::M()']]],
   ['mata_165',['matA',['../class_mech_model.html#ad7d1419f6416101208481cfe60ab9f98',1,'MechModel']]],
@@ -15,20 +14,4 @@ var searchData=
   ['mechtrap_2eh_174',['MechTrap.h',['../_mech_trap_8h.html',1,'']]],
   ['mises2d_175',['Mises2D',['../class_iso_hard.html#adf591629d4c2c294808d4e58a777997b',1,'IsoHard']]],
   ['mises3d_176',['Mises3D',['../class_iso_hard.html#aabc311f27028b7b16eae26caef7040b4',1,'IsoHard']]]
-=======
-  ['m_154',['m',['../class_mech_trap.html#a45f233faa9ac2dbdbdeda4a3028d0302',1,'MechTrap']]],
-  ['m_155',['M',['../class_transport_model.html#a81aef96bb4213714e09312044af65d52',1,'TransportModel']]],
-  ['mata_156',['matA',['../class_mech_model.html#ad7d1419f6416101208481cfe60ab9f98',1,'MechModel']]],
-  ['materialmodel_157',['materialModel',['../class_base_elem_mech.html#af562acf4ca0be34275c8169a351b1c4f',1,'BaseElemMech']]],
-  ['matk_158',['matK',['../class_trapping_model.html#afd6d88018e4cfc97aa4fe645a13ae1bb',1,'TrappingModel']]],
-  ['matm_159',['matM',['../class_trapping_model.html#a3b356fdf1cb94064e089dbbf1f5a450b',1,'TrappingModel']]],
-  ['matrix_2eh_160',['Matrix.h',['../_matrix_8h.html',1,'']]],
-  ['max_5fiter_161',['max_iter',['../class_iso_hard.html#a299c28be9b30e2f1df761a5ed6a56b5a',1,'IsoHard::max_iter()'],['../class_mech_model.html#ac8d35396489406be1f03b6c81a882011',1,'MechModel::max_iter()']]],
-  ['mechmodel_162',['MechModel',['../class_mech_model.html',1,'MechModel'],['../class_mech_model.html#a61d582150747bd014c6f303b28b4801b',1,'MechModel::MechModel()']]],
-  ['mechmodel_2eh_163',['MechModel.h',['../_mech_model_8h.html',1,'']]],
-  ['mechtrap_164',['MechTrap',['../class_mech_trap.html',1,'MechTrap'],['../class_mech_trap.html#ad4cff52ad268ac7d915b3cd5c569c57d',1,'MechTrap::MechTrap()']]],
-  ['mechtrap_2eh_165',['MechTrap.h',['../_mech_trap_8h.html',1,'']]],
-  ['mises2d_166',['Mises2D',['../class_iso_hard.html#adf591629d4c2c294808d4e58a777997b',1,'IsoHard']]],
-  ['mises3d_167',['Mises3D',['../class_iso_hard.html#aabc311f27028b7b16eae26caef7040b4',1,'IsoHard']]]
->>>>>>> 923adef008afcaadba43b8919049c25bc60af2cd
 ];
